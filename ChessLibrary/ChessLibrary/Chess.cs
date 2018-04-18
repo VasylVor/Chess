ce60009795534ab5a779d2f39e8037a92d6aeb5e
VasyaVor <vasilvoronin0@gmail.com>
2018-04-18 17:42:34 +0300

    first test

---
+++ ChessLibrary/ChessLibrary/Chess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLibrary
+{
+    /// <summary>
+    /// Library for chess rules
+    /// </summary>
+    public class Chess
+    {
+        public string Fen { get; private set; }
+
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        {
+            Fen = fen;
+        }
+
+        public Chess Move(string move)// приймає хід в вигляді PE2E4  PE7E8
+        {
+            Chess nextCheess = new Chess(Fen);
+            return nextCheess;
+        }
+
+        public char GetFigureAt(int x, int y)
+        {
+            return '.';
+        }
+    }
+}
---
+++ ChessLibrary/DemoChess/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessLibrary;
+
+namespace DemoChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chess chess = new Chess();
+            while (true)
+            {
+                Console.WriteLine(chess.Fen);
+                string move = Console.ReadLine();
+                if (move == "") break;
+                chess = chess.Move(move);
+            }
+        }
+    }
+}
